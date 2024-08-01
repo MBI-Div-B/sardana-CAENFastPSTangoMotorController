77785f75a156b7ad268d7d2e685899f170cafe90
MOKE2 user <moke2@moke2.lab>
2024-08-01 17:22:45 +0200

    modifications for updated pytango device

--- CAENFastPSTangoMotorController.py
+++ CAENFastPSTangoMotorController.py
@@ -1,17 +1,29 @@
 from sardana import State
 from sardana.pool.controller import MotorController
-from sardana.pool.controller import Type, Description, DefaultValue
+from sardana.pool.controller import Type, Description, DefaultValue, Access, DataAccess
 
 from tango import DeviceProxy
 import time
 
 class CAENFastPSTangoMotorController(MotorController):
+    MaxDevice = 2
+    default_acceleration_time = 0
+    default_base_rate = 0
+    default_threshold = 0.001
+    default_move_grace_time = 3
+    
     ctrl_properties = {'tangoFQDN': {Type: str,
                                      Description: 'The FQDN of the CAEN FastPS Tango DS',
                                      DefaultValue: 'domain/family/member'},
                        }
     
-    MaxDevice = 2
+    axis_attributes = {
+        "Threshold": {
+            Type: float,
+            Description: "max. allowed deviation from target position",
+            DefaultValue: 0.001,
+        },
+    }
     
     def __init__(self, inst, props, *args, **kwargs):
         super(MotorController, self).__init__(
@@ -21,30 +33,41 @@ class CAENFastPSTangoMotorController(MotorController):
         self.proxy = DeviceProxy(self.tangoFQDN)
         print('SUCCESS')
         self._timeout = 10
-        self._threshold = 0.0001
+        self._update_mode = None
         self._motors = {}
         
     def AddDevice(self, axis):
         self._motors[axis] = {}
-        self._motors[axis]['is_moving'] = None
-        self._motors[axis]['move_start_time'] = None
-        self._motors[axis]['target'] = None
+        self._motors[axis]['is_moving'] = False
+        self._motors[axis]['move_start_time'] = 0
+        self._motors[axis]['threshold'] = self.default_threshold
+        self._motors[axis]['timeout'] = 0
+        pos = self.ReadOne(axis)
+        self._motors[axis]['target'] = pos
+        self.proxy.ramping = True
 
     def DeleteDevice(self, axis):
         del self._motors[axis]
+
+    def PreStateAll(self):
+        self._update_mode = self.proxy.update_mode
 
     def StateOne(self, axis):
         limit_switches = MotorController.NoLimitSwitch
         
         pos = self.ReadOne(axis)
+        target = self._motors[axis]['target']
+        threshold = self._motors[axis]['threshold']
+        start_time = self._motors[axis]['move_start_time']
+        timeout = self._motors[axis]['timeout']
         now = time.time()
         
         try:
             if self._motors[axis]['is_moving'] == False:
                 state = State.On
-            elif self._motors[axis]['is_moving'] & (abs(pos-self._motors[axis]['target']) > self._threshold): 
+            elif self._motors[axis]['is_moving'] & (abs(pos - target) > threshold): 
                 # moving and not in threshold window
-                if (now-self._motors[axis]['move_start_time']) < self._timeout:
+                if (now - start_time) < timeout:
                     # before timeout
                     state = State.Moving
                 else:
@@ -52,13 +75,14 @@ class CAENFastPSTangoMotorController(MotorController):
                     self._log.warning('CAEN FAST-PS Timeout')
                     self._motors[axis]['is_moving'] = False
                     state = State.On
-            elif self._motors[axis]['is_moving'] & (abs(pos-self._motors[axis]['target']) <= self._threshold): 
+            elif self._motors[axis]['is_moving'] & (abs(pos - target) <= threshold): 
                 # moving and within threshold window
                 self._motors[axis]['is_moving'] = False
                 state = State.On
             else:
                 state = State.Fault
-        except:
+        except Exception as exc:
+            print(f"Exception is StateOne: {exc}")
             state = State.Fault
         
         return state, 'all fine', limit_switches
@@ -68,8 +92,13 @@ class CAENFastPSTangoMotorController(MotorController):
             return self.proxy.current
         else:
             return self.proxy.voltage
+    
+    def PreStartOne(self, axis, value):
+        return self.proxy.update_mode == self.proxy.update_mode.NORMAL
 
     def StartOne(self, axis, position):
+        start_position = self.ReadOne(axis)
+        velocity = self.GetAxisPar(axis, "velocity")
         if axis == 0:
             self.proxy.current = position
         else:
@@ -77,13 +106,56 @@ class CAENFastPSTangoMotorController(MotorController):
         self._motors[axis]['move_start_time'] = time.time()
         self._motors[axis]['is_moving'] = True
         self._motors[axis]['target'] = position
-
-    def StopOne(self, axis):
-        pass
+        self._motors[axis]["timeout"] = abs(position - start_position) / velocity + self.default_move_grace_time
 
     def AbortOne(self, axis):
-        pass
+        current_position = self.ReadOne(axis)
+        if axis == 0:
+            self.proxy.current = current_position
+        else:
+            self.proxy.voltage = current_position
+        self._motors[axis]['is_moving'] = False
+        self._motors[axis]['target'] = current_position
+
+    def GetAxisPar(self, axis, name):
+        name = name.lower()
+        if name == "acceleration":
+            v = self.default_acceleration_time
+        elif name == "deceleration":
+            v = self.default_acceleration_time
+        elif name == "base_rate":
+            v = self.default_base_rate
+        elif name == "velocity":
+            if axis == 0:
+                v = self.proxy.ramp_current
+            else:
+                v = self.proxy.ramp_voltage
+        elif name == "step_per_unit":
+            v = 1
+        return v
+
+    def SetAxisPar(self, axis, name, value):
+        name = name.lower()
+        if name == "acceleration":
+            pass
+        elif name == "deceleration":
+            pass
+        elif name == "base_rate":
+            pass
+        elif name == "velocity":
+            if axis == 0:
+                self.proxy.ramp_current = value
+            else:
+                self.proxy.ramp_voltage = value
+        elif name == "step_per_unit":
+            pass
     
+    def getThreshold(self, axis):
+        return self._motors[axis]["threshold"]
+    
+    def setThreshold(self, axis, value):
+        self._motors[axis]["threshold"] = value
+
     def SendToCtrl(self, cmd):
         """
         Send custom native commands. The cmd is a space separated string
